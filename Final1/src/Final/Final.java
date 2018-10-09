18c8f6b239be4b0ae6964e7ef8003ee7fbfe9bf0
Tavius <js138436@students.davenport.k12.ia.us>
2018-10-09 08:42:12 -0500

    updated final

--- Final1/src/Final/Final.java
+++ Final1/src/Final/Final.java
@@ -12,8 +12,8 @@ public class Final {
 	        System.out.println("Welcome to the best game ever Skeleton wars, that has no skeletons possibly you find out yourself. ");
 	        System.out.println("Choices will be either 1-4, hoit those buttons for choices.");
 	        
-	        System.out.println("Story: You wake up in a barely lit room, there seems to be only one door, after exitint said door"
-	        		+ "Enter a dark hallway, walking for minutes after running into a old wooden door nearly breaking it,"
+	        System.out.println("Story: You wake up in a barely lit room, there seems to be only one door, after exiting said door"
+	        		+ "Enter a dark hallway, walking for minutes \nafter running into a old wooden door nearly breaking it,"
 	        		+ "after entering there is a hallway and two others doors across from each other, in the middle"
 	        		+ "a skeleton t poses, choose your quest.  ");
 		
