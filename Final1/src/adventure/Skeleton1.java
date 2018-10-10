0fa790a08b353d99630090158384ef7f86a824b6
Orion03 <42647294+Orion03@users.noreply.github.com>
2018-10-10 09:11:15 -0500

    updated text

--- Final1/src/adventure/Skeleton1.java
+++ Final1/src/adventure/Skeleton1.java
@@ -1,15 +1,24 @@
 package adventure;
 
+import java.util.Scanner;
+
 public class Skeleton1 {
 
 	public static void main(String[] args) {
+		
+		text();
 
 	}
 	
 	public static void text() {
 		
+		Scanner input = new Scanner(System.in);
+		
+		String input4;
+		
 		System.out.println("And thus, your adventure as a Skeleton Warrior begins...");
-		System.out.println("				Press any key to continue				");
+		System.out.println("		Press any key to continue");
+		input4 = input.next();
 		
 	}
 
