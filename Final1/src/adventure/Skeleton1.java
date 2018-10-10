3eeb5c6cdf0c949bcefb826a5fc44285e8c729ee
Orion03 <42647294+Orion03@users.noreply.github.com>
2018-10-10 09:20:31 -0500

    updated text

--- Final1/src/adventure/Skeleton1.java
+++ Final1/src/adventure/Skeleton1.java
@@ -15,13 +15,21 @@ public class Skeleton1 {
 		Scanner input = new Scanner(System.in);
 		
 		String input4;
+		String input5;
 		
 		System.out.println("And thus, your adventure as a Skeleton Warrior begins...");
 		System.out.println("		   Rise from thy grave");
 		System.out.println("		Press any key to continue");
 		input4 = input.next();
 		
-		System.out.println("You're walking through the graveyard, hunting for the exit. However");
+		System.out.println("You walk through the graveyard, searching for an exit. Through your search, \nsomething catches your eye socket. It's a frog. A cool frog. \n \nApproach the "
+				+ "cool frog?");
+		input5 = input.next();
+		
+		if(input5 == "Yes")
+		{
+			System.out.println("e");
+		}
 		
 	}
 
