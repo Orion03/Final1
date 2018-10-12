2b1c9f469ec5b2d006fd00b69804e00ed33a1686
Orion03 <42647294+Orion03@users.noreply.github.com>
2018-10-12 09:35:42 -0500

    updated text

--- Final1/src/finalChapter/Skeleton1FinalChapter.java
+++ Final1/src/finalChapter/Skeleton1FinalChapter.java
@@ -16,6 +16,7 @@ public class Skeleton1FinalChapter {
 		
 		int input8;
 		String input9;
+		int input10;
 		
 		System.out.println("The Final Chapter Begins...");
 		System.out.println("Your standing at the exit. It's 4:30pm, October 31st. 30 minutes early. What humans call .:Trick-or-treating:. "
@@ -34,13 +35,23 @@ public class Skeleton1FinalChapter {
 			
 			if(input9.equalsIgnoreCase("Yes") || input9.equalsIgnoreCase("y"))
 			{
-				System.out.println("You decided to raid the grave. You've disturbed the spirit of that grave, and you feel uneasy. You "
+				System.out.println("You decide to raid the grave. You've disturbed the spirit of that grave, and you feel uneasy. You "
 						+ "realize that was actually Elvis Presley's grave. He decides that you disrupted him and is punishable by death. "
 						+ "You die on the spot, just from him looking at you. You died. Start your adventure over.");
 			}
 			
 			if(input9.equalsIgnoreCase("No") || input9.equalsIgnoreCase("n"))
 			{
+				System.out.println("You decide not to disrupt the dead. A good decision on your part. Luckily though, there are convient "
+						+ "daggers right by the grave that don't belong to anybody. You decide to pick them up. Now that you have another "
+						+ "type of weapon, it's time to gather the other things:");
+				System.out.println("\n1 - Find healing items \n2 - Enhance strength with milk");
+				input10 = input.nextInt();
+				
+				if(input10 == 1)
+				{
+					System.out.println("You decide to search for healing items next");
+				}
 				
 			}
 		}
